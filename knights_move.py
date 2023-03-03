13eeed0618d7d513b00f5c5e150d521ef126648a
kh.adelya <AKhusnullina@croc.ru>
2023-03-03 23:03:09 +0300

    dynamic programming with two parameters

---
+++ knights_move.py
@@ -0,0 +1,15 @@
+with open("input.txt", "r") as file:
+    N, M = list(map(int, file.read().split()))
+    if N == M == 1:
+        print(1)
+    else:
+        board = [[0 for _ in range(M)] for _ in range(N)]
+        if N >= 3 and M >= 2:
+            board[2][1] = 1
+        if M >= 3 and N >= 2:
+            board[1][2] = 1
+        if N >= 3 and M >= 3:
+            for i in range(2, N):
+                for j in range(2, M):
+                    board[i][j] = board[i - 1][j - 2] + board[i - 2][j - 1]
+        print(board[-1][-1])
