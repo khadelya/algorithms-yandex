69481b788edd43757f1344e841825dbc465b70fc
kh.adelya <AKhusnullina@croc.ru>
2023-03-13 16:22:52 +0300

    matrix element sum

--- sum_in_the_rectangle.py
+++ sum_in_the_rectangle.py
@@ -1,16 +1,26 @@
 with open("input.txt", "r") as file:
     contents = file.readlines()
     N, M, K = list(map(int, contents[0].split()))
-    matrix = []
+    matrix = [[0] * (M + 1) for i in range(N + 1)]
     n = 1
     for i in range(1, N + 1):
-        matrix.append(list(map(int, contents[n].split())))
+        matrix[i] = [0] + list(map(int, contents[n].split()))
         n += 1
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            matrix[i][j] = (
+                matrix[i][j]
+                + matrix[i][j - 1]
+                + matrix[i - 1][j]
+                - matrix[i - 1][j - 1]
+            )
     for i in range(K):
-        matrix_sum = 0
         x1, y1, x2, y2 = list(map(int, contents[n].split()))
-        for i in range(x1 - 1, x2):
-            for j in range(y1 - 1, y2):
-                matrix_sum += matrix[i][j]
-        print(matrix_sum)
+        ans = (
+            matrix[x2][y2]
+            - matrix[x1 - 1][y2]
+            - matrix[x2][y1 - 1]
+            + matrix[x1 - 1][y1 - 1]
+        )
+        print(ans)
         n += 1
